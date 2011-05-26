e95a485682085c6b4bceeebd54dfe72baea0a29b
Simon McManus <mcmanus.simon@gmail.com>
2011-05-26 22:03:24 +0100

    working with layout.html

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-var sizlate = require('./sizlate.js');
+var sizlate = require('./sizlate1.js');
 var app = require('express').createServer();
 
 app.set( "view engine", "html" );
-app.register( ".html", sizlate);
+
+app.register('.html', {
+    compile: function(str, options) {
+        return function(locals) {
+            return sizlate.render(str, {locals: locals});
+        }
+    }
+});
 
 app.get('/', function(req, res){
-	res.render('pageTemplate', {
+	res.render('page1.html', {
 		locals: {
 			selectors: {
-				'h1':'REPLACED: template test title',
-				'#1':'bad dog',
+				'h1:first':'REPLACED: template test title',
+				'h2':'replaced h2'
+			
+				,
 				'ul li': ['item 1', 'item2', 'item3']
+				
 			}
 		}
-	});sd
+	});
 });
 
 app.listen(8000);
