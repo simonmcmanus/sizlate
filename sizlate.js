7d50d12a20eb295e59959ddd8e2eb5bed659256b
Simon McManus <mcmanus.simon@gmail.com>
2013-01-21 17:51:43 +0000

    fix with multiple partials

--- sizlate.js
+++ sizlate.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var cheerio = require('cheerio');
-exports.version = '0.7.6';
+exports.version = '0.7.7';
 
 var checkForInputs = function($node, data) {
 	$node.each(function(i, elem) {
@@ -90,9 +90,9 @@ exports.__express = function(filename, options, callback) {
 	for(var key in selectors) {
 		if(selectors[key] && selectors[key].partial){// this is a partial.
 			if(selectors[key].data && selectors[key].data.length > 0){ // make sure we are passed in data and that the data is not empty.
-				fs.readFile(options.settings.views + '/partials/' + selectors[key].partial + '.' + options.settings['view engine'], 'utf8', function (err, data) {
+				fs.readFile(options.settings.views + '/partials/' + selectors[key].partial + '.' + options.settings['view engine'], 'utf8', function (key, err, data) {
 					selectors[key] = exports.doRender('<body>' + data + '</body>', exports.classifyKeys(selectors[key].data, selectors[key])).slice(6, -7);	// adding and then stripping body tag for jsdom.
-				});
+				}.bind({}, key));
 			}
 		}
 	}
