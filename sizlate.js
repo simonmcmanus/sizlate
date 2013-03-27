e549471a12c6048c18b6d82eeccbeec33c42f274
Simon McManus <mcmanus.simon@gmail.com>
2013-03-27 12:17:21 +0000

    broken serverside

--- sizlate.js
+++ sizlate.js
@@ -1,122 +1,157 @@
-var fs = require('fs');
-var cheerio = require('cheerio');
-exports.version = '0.7.13';
+// var fs = require('fs');
+// var cheerio = require('cheerio');
+// exports.version = '0.9.0';
+var exports = {};
 
-var checkForInputs = function($node, data) {
-	$node.each(function(i, elem) {
-		if(this[0].name === 'input') {
-			$(this[0]).attr('value', data);
-		}else {
-			$(this[0]).html(data);
-		}
+var dom = function(str) {
+	return $(str);
+};
+
+
+var get = function( file, callback ) {
+	fs.readFile(options.settings.views + file, 'utf8', callback);
+};
+/*
+
+always called relative to the view directory
+ */
+var get = function(file, callback) {
+	console.log('file', file);
+	$.get('/targets/views/' + file  + '.sizlate', function(markup) {
+		callback(null, markup);
 	});
 };
-var updateNode = function($node, selector, data) {
-	// if there are multiple using the same selector then need to be addressed here.
-	switch(typeof data) {
-		case "string":
-			if(data !== ""){
-				checkForInputs($node, data);
-			}
-		break;
-		case "number": // TODO - confirm - this seems wrong - why only numbers to ids?
-			if(selector == ".id"){
-				$node.attr('id', data);
-			}else if(selector == ".data-id") {
-				$node.attr('data-id', data);
+
+
+(function(exports, dom, get) {
+	var checkForInputs = function($node, data) {
+		$node.each(function(i, elem) {
+			if(this[0] && this[0].name === 'input') {
+				$(this[0]).attr('value', data);
 			}else {
-				checkForInputs($node, data);
+				$(this[0]).html(data);
 			}
-		break;
-		case "object":
-			for(var key in data){
-
-				if(key === 'selectors') { // allow nested selectors
-					$node.html(exports.doRender($node.html(), data[key]));
+		});
+	};
+	var updateNode = function($node, selector, data) {
+		switch(typeof data) {
+			case "string":
+				if(data !== ""){
+					checkForInputs($node, data);
 				}
-				if(key == 'className'){
-					$node.addClass(data[key]);
+			break;
+			case "number": // TODO - confirm - this seems wrong - why only numbers to ids?
+				if(selector == ".id"){
+					$node.attr('id', data);
+				}else if(selector == ".data-id") {
+					$node.attr('data-id', data);
 				}else {
-					$node.attr(key, data[key]);
+					checkForInputs($node, data);
+				}
+			break;
+			case "object":
+				for(var key in data){
+					if(key === 'selectors') { // allow nested selectors
+						$node.html(exports.doRender($node.html(), data[key]));
+					}
+					if(key == 'className'){
+						$node.addClass(data[key]);
+					}else {
+						$node.attr(key, data[key]);
+					}
+				}
+			break;
+		}
+		return $node;
+	};
+
+	var selectorIterator = function(selectors, $html) {
+		for(var selector in selectors) {
+			if(typeof selectors[selector] === 'function') {
+				break;
+			}
+			var $domNode = $html.find(selector);
+			if($domNode) {
+				$domNode = updateNode($domNode, selector, selectors[selector]);
+			}
+		}
+	};
+
+	exports.classifyKeys = function(data, options) {
+		if(!options.classifyKeys || typeof data == "undefined"){
+			return data;
+		}
+		var c = data.length;
+		var retArray = [];
+		while(c--) {
+			var newObj = {};
+			for(var key in data[c]){
+				newObj['.'+key] = data[c][key];
+			}
+			retArray.push(newObj);
+		}
+		return retArray;
+	};
+
+	exports.doRender = function(str, selectors) {
+		if(!selectors){
+			return str;
+		}
+		var selectors = ( typeof selectors[0] == 'undefined' ) ? [selectors] : selectors; // make sure we have an array.
+		var selectorCount = selectors.length;
+		var out = [];
+		while(selectorCount--){
+			sizzled = $(str);
+			selectorIterator(selectors[selectorCount], sizzled);
+			out.push(sizzled.html());
+		}
+		return out.join('');
+	};
+
+	exports.__express = function(filename	, options, callback) {
+
+		var selectors = options.selectors;
+		var wait = false;
+		var count = 0; // keep track of total number of callbacks to wait for
+		var complete = 0; // completed callbacks count.
+		for(var key in selectors) {
+			if(selectors[key] && selectors[key].partial){// this is a partial.
+				if(selectors[key].data && selectors[key].data.length > 0){ // make sure we are passed in data and that the data is not empty.
+					wait = true;
+					count++;
+					get('/partials/' + selectors[key].partial + '.sizlate', function (key, err, data) {
+						selectors[key] = exports.doRender(data, exports.classifyKeys(selectors[key].data, selectors[key]));	// adding and then stripping body tag for jsdom.
+						complete++;
+						if(complete === 1) {
+							doRendering();
+						}
+					}.bind({}, key));
 				}
 			}
-		break;
-	}
-	return $node;
-};
-
-var selectorIterator = function(selectors, $) {
-	for(var selector in selectors) {
-		var $domNode = $(selector);
-		if($domNode) {
-			$domNode = updateNode($domNode, selector, selectors[selector]);
 		}
-	}
-};
 
-exports.classifyKeys = function(data, options) {
-	if(!options.classifyKeys || typeof data == "undefined"){
-		return data;
-	}
-	var c = data.length;
-	var retArray = [];
-	while(c--) {
-		var newObj = {};
-		for(var key in data[c]){
-			newObj['.'+key] = data[c][key];
-		}
-		retArray.push(newObj);
-	}
-	return retArray;
-};
-
-exports.doRender = function(str, selectors) {
-	if(!selectors){
-		return "";
-	}
-	var selectors = ( typeof selectors[0] == 'undefined' ) ? [selectors] : selectors; // make sure we have an array.
-	var selectorCount = selectors.length;
-	var out = [];
-	while(selectorCount--){
-		$ = cheerio.load(str);
-		selectorIterator(selectors[selectorCount], $);
-		out.push($.html());
-	}
-	return out.join('');
-};
-
-exports.__express = function(filename, options, callback) {
-	var fs = require('fs');
-	var selectors = options.selectors;
-	for(var key in selectors) {
-		if(selectors[key] && selectors[key].partial){// this is a partial.
-			if(selectors[key].data && selectors[key].data.length > 0){ // make sure we are passed in data and that the data is not empty.
-				fs.readFile(options.settings.views + '/partials/' + selectors[key].partial + '.sizlate', 'utf8', function (key, err, data) {
-					selectors[key] = exports.doRender(data, exports.classifyKeys(selectors[key].data, selectors[key]));	// adding and then stripping body tag for jsdom.
-				}.bind({}, key));
+		var doRendering = function() {
+			if(options.layout) {
+				get('/' + options.layout + '.'+ options.settings['view engine'], function(error, template) {
+					get(filename, function(err,data){
+					  var selectors = {};
+					  selectors[options.container || '#container'] = data;
+					  var markup = exports.doRender(template,  selectors);
+					  callback(null, exports.doRender(markup, options.selectors));
+					});
+				});
+			} else { // no layouts specified, just do the render.
+				get(filename, function(err,data){
+					var out = exports.doRender(data, options.selectors);
+				  callback(null, out);
+				});
 			}
 		}
-	}
-	if(options.layout) {
-		fs.readFile(options.settings.views + '/' + options.layout + '.'+ options.settings['view engine'], 'utf8', function(error, template) {
-			fs.readFile(filename, 'utf8', function(err,data){
-			  if(err) {
-			    console.error("Could not open file: %s", err);
-			    process.exit(1);
-			  }
-			  var selectors = {};
-			  selectors[options.container || '#container'] = data;
-			  var markup = exports.doRender(template,  selectors) ;
-			  callback(null, exports.doRender(markup, options.selectors));
-			});
-		});
-	} else { // no layouts specified, just do the render.
-		fs.readFile(filename, 'utf8', function(err,data){
-		  if(err) {
-		    console.error("Could not open file: %s", err);
-		    process.exit(1);
-		  }
-		  callback(null, exports.doRender(data, options.selectors)	 );
-		});
-	}
-};
+		if(!wait) {
+			doRendering();
+		}
+	};
+	exports.interface = exports.__express;
+
+
+})(exports, dom, get);
