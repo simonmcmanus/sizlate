b5b91cc1808f488a6a1499ba4996fb5e16cf1470
Codeship CI <mcmanus.simon@gmail.com>
2015-10-07 08:26:49 +0100

    added defau;lt

--- spec/multiple.spec.js
+++ spec/multiple.spec.js
@@ -15,32 +15,31 @@ describe('When given a regex', function() {
 		var expected = '<div class="one">new value</div><div class="one">new something</div>';
 		expect(out).toEqual(expected);
 	});
-//
-	// it("it should allow prepending a URL", function(done) {
-	// 	var out = sizlate.doRender('<a href="/some-path">existing value</a>', {
-	// 		'a': {
-	// 			'href' : {
-	// 				regex : /(.+)/ig,
-	// 				value : "http://yahoo.com$1"
-	// 			}
-	// 		}
-	// 	});
-	// 	var expected = '<a href="http://yahoo.com/some-path">existing value</a>';
-	// 	expect(out).toEqual(expected);
-	// 	done();
-	// });
+
+	it("it should allow prepending a URL", function(done) {
+		var out = sizlate.doRender('<a href="/some-path">link</a>', {
+			'a': {
+				'href' : {
+					regex : /(.+)/ig,
+					value : "http://yahoo.com$1"
+				}
+			}
+		});
+		var expected = '<a href="http://yahoo.com/some-path">link</a>';
+		expect(out).toEqual(expected);
+		done();
+	});
 });
 
-
-// describe('When given a function', function() {
-// 	it("it should allow anything you like", function(done) {
-// 		var out = sizlate.doRender('<div class="one">value A</div><div class="one">value B</div>', {
-// 			'.one': {
-// 				'innerText' : function( text ){ return text.replace(/value/ig,"amaze"); }
-// 			}
-// 		});
-// 		var expected = '<div class="one">amaze A</div><div class="one">amaze B</div>';
-// 		expect(expected).toEqual(out);
-// 		done();
-// 	});
-// });
+describe('When given a function', function() {
+	it("it should allow anything you like", function(done) {
+		var out = sizlate.doRender('<div class="one">value A</div><div class="one">value B</div>', {
+			'.one': {
+				'innerText' : function( text ){ return text.replace(/value/ig,"amaze"); }
+			}
+		});
+		var expected = '<div class="one">amaze A</div><div class="one">amaze B</div>';
+		expect(expected).toEqual(out);
+		done();
+	});
+});
