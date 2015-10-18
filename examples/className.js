8236d8d78f9005660168accc4272bf87a4bba2a4
Codeship CI <mcmanus.simon@gmail.com>
2015-10-18 12:35:45 +0100

    moved out express examples and added some nice simple ones

---
+++ examples/className.js
@@ -0,0 +1,10 @@
+var sizlate = require('sizlate');
+
+var html = '<div><a class="class1"></a></div>';
+var selectors = {
+    'div a': {
+        className: 'class2'
+    }
+};
+var out = sizlate.render(html, selectors);
+console.log(out);
