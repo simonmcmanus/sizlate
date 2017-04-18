2de66857a91ee3ed755d04f2ce1702b0fc2f8b01
Simon McManus <mcmanus.simon@gmail.com>
2017-04-18 20:43:10 +0100

    tweaking false behaviour

--- spec/boolean-spec.js
+++ spec/boolean-spec.js
@@ -3,10 +3,24 @@ if(typeof require != 'undefined') {
 }
 
 describe('When a false value', function() {
+    it("it should remove the node", function() {
+        var html = '<div><h1></h1></div>';
+        var selectors = {
+            'h1': false
+        };
+
+        var out = sizlate.render(html, selectors);
+        var expected = '<div></div>';
+        expect(out).toEqual(expected);
+     });
+});
+
+
+describe('When a false array', function() {
     it("it should remove the parent", function() {
         var html = '<div><ul><li></li></ul></div>';
         var selectors = {
-            'li': false
+            'li': [ false ]
         };
 
         var out = sizlate.render(html, selectors);
