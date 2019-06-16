fbe02ba601a01c4915bba16e32d4bd5e657172cb
Simon McManus <mcmanus.simon@gmail.com>
2019-06-16 08:15:17 +0100

    firefox 66

--- sizlate-client.js
+++ sizlate-client.js
@@ -93,7 +93,7 @@ exports.newValue = function ($node, selectors) {
 var dom = require('../server/dom')
 /**
  * In the case of input we should update the value and not just set the innerHTML property.
- * @param  {Object} $node sizzle object
+ * @param  {Object} $node selector object
  * @param  {String} data  The value to be set on the html.
  */
 module.exports = function ($node, data) {
