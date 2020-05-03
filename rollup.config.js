54de42cc67674cc81c0a09f0579506e1ea3021b9
Simon McManus <mcmanus.simon@gmail.com>
2020-05-03 10:09:28 +0100

    remove browser and unpkg field as they are not format as per spec

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,19 @@ import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
 import alias from '@rollup/plugin-alias';
 import minify from 'rollup-plugin-babel-minify'
-
+const plugins = [
+	alias({
+		entries: [
+			{ find: '../server/dom', replacement: '../client/dom' }
+		
+		]
+	}),
+	resolve(), // so Rollup can find node modules
+	commonjs(), // so Rollup can convert node modules to an ES module
+	minify({
+		comments: false
+	  }),
+]
 export default [
 	{  // client side consumable es module containing all modules
 		input: 'sizlate.js',
@@ -12,18 +24,15 @@ export default [
 			file: pkg.module,
             format: 'es' 
 		},
-		plugins: [
-            alias({
-                entries: [
-                    { find: '../server/dom', replacement: '../client/dom' }
-                
-                ]
-			}),
-			resolve(), // so Rollup can find node modules
-            commonjs(), // so Rollup can convert node modules to an ES module
-			minify({
-				comments: false
-			  }),
-		]
+		plugins 
+	},
+	{  // client side consumable es module containing all modules
+		input: 'sizlate.js',
+		output: {
+			name: 'sizlate',
+			file: 'sizlate-module-min.mjs',
+            format: 'es' 
+		},
+		plugins: [...plugins, minify()] 
 	}
 ];
